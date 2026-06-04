21751645dd051bcf499d8d39a62ad9a145168cb9
Suzuhigashi <=2026053@fsc-educ.com>
2026-06-04 14:49:59 +0900

    blogページの変更

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,9 +1,9 @@
 package com.example.blog_app;
 
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
